55f32ce614c544151c31697f6166776eb971b256
nguyenhung <hungxt232467@gmail.com>
2022-04-22 09:16:45 +0700

    update

---
+++ demo/bai73.java
@@ -0,0 +1,13 @@
+public class bai73 {
+    public static void main(String[] args) {
+        int number1 = 11;
+        int number4 = 44;
+        int number5 = 55;
+        int number2 = 22;
+        int number3 = 33;
+        int sum;
+        sum = (number1 * 2) + (number2 * 1) + (number3 * 2) + (number4 * 3) + (number5 * 4);
+        System.out.print("The sum is ");
+        System.out.println(sum);
+    }
+}
